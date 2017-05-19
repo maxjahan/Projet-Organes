33f0de7b51ca97b409571b168b32853b463a8e1f
Laetitia Dupret <laetitia.dupret@student.uclouvain.be>
2017-05-19 17:16:26 +0200

    Chgt ankle

--- Ankle.m
+++ Ankle.m
@@ -1,4 +1,4 @@
-
+close all
 
 a_ankle=0;
 b_ankle=0;
@@ -8,7 +8,11 @@ sommet_plat_ankle=0;
 moyenne_sommet_plat_ankle=0;
 minima_plat_ankle=0;
 moyenne_minima_plat_ankle=0;
-ankleChangement = false;
+ankleIsNot2 = false;
+ankleIs3 = false;
+
+sommet_plat_ankle_max=0;
+sommet_plat_ankle_min=2000;
 
 BMI=(sub.weight)/(sub.height)^2;
 l = length(time_vect);
@@ -17,73 +21,129 @@ answerFinale = zeros(1,l);
 
 
 for i=1:l
-    
     %% Left Ankle
+    %     if i<700 %On suppose que le patient est sur du plat pendant au moins 7 sec
+    %         a_ankle=a_ankle+left_ankle(i); % pour pouvoir calculer la valeur moyenne
+    %         ankleChangement=true; % on est sur du plat
+    %         if i>2 && left_ankle(i)<left_ankle(i-1) && left_ankle(i-1)>left_ankle(i-2)%si c'est un sommet
+    %             b_ankle = b_ankle+1;
+    %             sommet_plat_ankle(b_ankle)=left_ankle(i);%matrice avec vleur des sommets
+    %         elseif i>2 && left_ankle(i)>left_ankle(i-1) && left_ankle(i-1)<left_ankle(i-2)%si c'est un minima
+    %             c_ankle = c_ankle+1;
+    %             minima_plat_ankle(c_ankle)=left_ankle(i);%matrice avec valeur des minima
+    %         end
+    %     elseif i==700
+    %         a_ankle=(a_ankle+left_ankle(i))/700;%valeur moyenne plat
+    %         answer_ankle=[1 1];
+    %         moyenne_sommet_plat_ankle = mean(sommet_plat_ankle);%valeur moyenne sommets sur plat
+    %         moyenne_minima_plat_ankle = mean(minima_plat_ankle);%valeur moyenne minima sur plat
+    %         left_ankle_2 = (left_ankle-a_ankle)/(moyenne_sommet_plat_ankle-moyenne_minima_plat_ankle);%on normalise la fonction (? faire au fur et ? mesure dans la version finale!!!!!)
+    %         figure
+    %         plot(left_ankle_2)
+    %         hold on
+    %         line([1 l],[0.1 0.1],'Color','red')
+    %         hold on
+    %         line([1 l],[0.2 0.2],'Color','red')
+    %         hold on
+    %         line([1 l],[0.3 0.3],'Color','red')
+    %         hold on
+    %         line([1 l],[0.4 0.4],'Color','red')
+    %         hold on
+    %         line([1 l],[0.5 0.5],'Color','red')
+    %         hold on
+    %         line([1 l],[0.6 0.6],'Color','red')
+    %         hold on
+    %         line([1 l],[-0.1 -0.1],'Color','red')
+    %         hold on
+    %         line([1 l],[-0.2 -0.2],'Color','red')
+    %         hold on
+    %         line([1 l],[-0.3 -0.3],'Color','red')
+    %         hold on
+    %         line([1 l],[-0.4 -0.4],'Color','red')
+    %         hold on
+    %         line([1 l],[-0.5 -0.5], 'Color','red')
+    %         title('left ankle')
+    %         %moyenne_sommet_plat = 1;
+    %         %moyenne_minima_plat = -1;
+    %
+    %     elseif left_ankle_2(i)<left_ankle_2(i-1) && left_ankle_2(i-1)>left_ankle_2(i-4)%si on a un maximum
+    %         if (sub.gender=='M' && BMI<21 && left_ankle_2(i)>0.84) || (sub.gender=='F' && left_ankle_2(i)>1.28) || (sub.gender=='M'&&BMI>21 && left_ankle_2(i)>0.65)
+    %             %on est sur d'etre sur du plat ou descente
+    %             ankleChangement=true;
+    %         else
+    %             % sinon on n'a pas de nouvelles infos
+    %             compteur_ankle = 0;
+    %         end
+    %         %else answerl(i)=answerl(i-1); %si le max n'est pas entre ces valeur on ne peut d?terminer avec certitude -> comme avant
+    
     if i<700 %On suppose que le patient est sur du plat pendant au moins 7 sec
         a_ankle=a_ankle+left_ankle(i); % pour pouvoir calculer la valeur moyenne
-        ankleChangement=true; % on est sur du plat
-        if i>2 && left_ankle(i)<left_ankle(i-1) && left_ankle(i-1)>left_ankle(i-2)%si c'est un sommet
-            b_ankle = b_ankle+1;
-            sommet_plat_ankle(b_ankle)=left_ankle(i);%matrice avec vleur des sommets
-        elseif i>2 && left_ankle(i)>left_ankle(i-1) && left_ankle(i-1)<left_ankle(i-2)%si c'est un minima
-            c_ankle = c_ankle+1;
-            minima_plat_ankle(c_ankle)=left_ankle(i);%matrice avec valeur des minima
+        ankleIsNot2=true; % on est sur du plat
+        if left_ankle(i)>sommet_plat_ankle_max
+            sommet_plat_ankle_max = left_ankle(i);
+        elseif left_ankle(i)<sommet_plat_ankle_min
+            sommet_plat_ankle_min = left_ankle(i);
         end
     elseif i==700
         a_ankle=(a_ankle+left_ankle(i))/700;%valeur moyenne plat
-        answer_ankle=[1 1];
-        moyenne_sommet_plat_ankle = mean(sommet_plat_ankle);%valeur moyenne sommets sur plat
-        moyenne_minima_plat_ankle = mean(minima_plat_ankle);%valeur moyenne minima sur plat
-        left_ankle_2 = (left_ankle-a_ankle)/(moyenne_sommet_plat_ankle-moyenne_minima_plat_ankle);%on normalise la fonction (? faire au fur et ? mesure dans la version finale!!!!!)
+        ankleIsNot2=true;
+        left_ankle_2 = (left_ankle-a_ankle)/(sommet_plat_ankle_max-sommet_plat_ankle_min);%on normalise la fonction (? faire au fur et ? mesure dans la version finale!!!!!)
         figure
         plot(left_ankle_2)
-        hold on
-        line([1 l],[0.1 0.1],'Color','red')
-        hold on
-        line([1 l],[0.2 0.2],'Color','red')
-        hold on
-        line([1 l],[0.3 0.3],'Color','red')
-        hold on
-        line([1 l],[0.4 0.4],'Color','red')
-        hold on
-        line([1 l],[0.5 0.5],'Color','red')
-        hold on
-        line([1 l],[0.6 0.6],'Color','red')
-        hold on
-        line([1 l],[-0.1 -0.1],'Color','red')
-        hold on
-        line([1 l],[-0.2 -0.2],'Color','red')
-        hold on
-        line([1 l],[-0.3 -0.3],'Color','red')
-        hold on
-        line([1 l],[-0.4 -0.4],'Color','red')
-        hold on
-        line([1 l],[-0.5 -0.5], 'Color','red')
-        title('left ankle')
-        %moyenne_sommet_plat = 1;
-        %moyenne_minima_plat = -1;
-        
+        title('left ankle-2')
     elseif left_ankle_2(i)<left_ankle_2(i-1) && left_ankle_2(i-1)>left_ankle_2(i-4)%si on a un maximum
         if (sub.gender=='M' && BMI<21 && left_ankle_2(i)>0.84) || (sub.gender=='F' && left_ankle_2(i)>1.28) || (sub.gender=='M'&&BMI>21 && left_ankle_2(i)>0.65)
             %on est sur d'etre sur du plat ou descente
-            ankleChangement=true;
+            ankleIsNot2=true;
         else
             % sinon on n'a pas de nouvelles infos
             compteur_ankle = 0;
         end
-        %else answerl(i)=answerl(i-1); %si le max n'est pas entre ces valeur on ne peut d?terminer avec certitude -> comme avant
+    elseif std(left_ankle_2(i-150:i))*BMI- std(left_ankle_2(1:150))*BMI > 2.1
+        %         if answerFinale(i-1) == 2 && compteur <100 %ne peut pas redescendre dans la seconde quand il est sur du plat
+        %             compteur_ankle = compteur_ankle+1;
+        %         else
+        %             ankleIs3 = true;
+        %             compteur_ankle = 0;
+        %         end
+        ankleIs3=true;
     end
     %else answerl(i) = answerl(i-1);
     
     %% AnswerFinale
-    if ankleChangement
-        answerFinale(i) = 1; %on rajoute 1 et 3 dans les propositions
+    if ankleIsNot2
+        answerFinale1(i) = 1; %on rajoute 1 et 3 dans les propositions
     else
-        answerFinale(i)= 0;
+        answerFinale1(i)= 0;
     end
-    ankleChangement=false;
+    ankleIsNot2=false;
+    if ankleIs3
+        answerFinale2(i) = 3; %on rajoute 1 et 3 dans les propositions
+    else
+        answerFinale2(i)= 0;
+    end
+    ankleIs3=false;
+end
+
+figure
+plot(time_vect,answerFinale1, '.-b')
+% hold on
+% plot(time_vect,true_manoeuvre, '--r')
+
+figure
+plot(time_vect,answerFinale2, '.-b')
+% hold on
+% plot(time_vect,true_manoeuvre, '--r')
+
+q = 150;
+l = length(time_vect);
+movingSTD = zeros(1, l-q);
+for p= q+1:l
+    movingSTD(p-q) = std(left_ankle_2(p-q:p))*BMI;
 end
 figure
-plot(time_vect,answerFinale, '.-b')
+plot(time_vect(151:end),movingSTD-movingSTD(1))
 hold on
-plot(time_vect,true_manoeuvre, '--r')
+%line([1 l-q],[15 15],'Color','red')
+grid on
+title('standard dev')
--- organe_version4.m
+++ organe_version4.m
@@ -24,6 +24,8 @@ compteur2 = 0;
 
 sommet_plat_max=0;
 sommet_plat_min=2000;
+sommet_plat_ankle_max=0;
+sommet_plat_ankle_min=2000;
 
 al=0;
 bl=0; %compteur minima
@@ -50,7 +52,7 @@ moyenne_minima_plat_ankle=0;
 answer_hanche_g = 0;
 answer_hanche_d = 0;
 kneeChangement=false;
-ankleChangement = false;
+ankleIsNot2 = false;
 
 answerFinale = zeros(1,l);
 
@@ -125,92 +127,92 @@ for i=1:l
     end
     
     %% Ankle
+%     if i<700 %On suppose que le patient est sur du plat pendant au moins 7 sec
+%         a_ankle=a_ankle+left_ankle(i); % pour pouvoir calculer la valeur moyenne
+%         ankleIsNot2=true; % on est sur du plat
+%         if i>2 && left_ankle(i)<left_ankle(i-1) && left_ankle(i-1)>left_ankle(i-2)%si c'est un sommet
+%             b_ankle = b_ankle+1;
+%             sommet_plat_ankle(b_ankle)=left_ankle(i);%matrice avec valeur des sommets
+%         elseif i>2 && left_ankle(i)>left_ankle(i-1) && left_ankle(i-1)<left_ankle(i-2)%si c'est un minima
+%             c_ankle = c_ankle+1;
+%             minima_plat_ankle(c_ankle)=left_ankle(i);%matrice avec valeur des minima
+%         end
+%     elseif i==700
+%         a_ankle=(a_ankle+left_ankle(i))/700;%valeur moyenne plat
+%         ankleIsNot2=true;
+%         moyenne_sommet_plat_ankle = mean(sommet_plat_ankle);%valeur moyenne sommets sur plat
+%         moyenne_minima_plat_ankle = mean(minima_plat_ankle);%valeur moyenne minima sur plat
+%         left_ankle_2 = (left_ankle-a_ankle)/(moyenne_sommet_plat_ankle-moyenne_minima_plat_ankle);%on normalise la fonction (? faire au fur et ? mesure dans la version finale!!!!!)
+% %         figure
+% %         plot(left_ankle_2)
+% %         hold on
+% %         line([1 l],[0.1 0.1],'Color','red')
+% %         hold on
+% %         line([1 l],[0.2 0.2],'Color','red')
+% %         hold on
+% %         line([1 l],[0.3 0.3],'Color','red')
+% %         hold on
+% %         line([1 l],[0.4 0.4],'Color','red')
+% %         hold on
+% %         line([1 l],[0.5 0.5],'Color','red')
+% %         hold on
+% %         line([1 l],[0.6 0.6],'Color','red')
+% %         hold on
+% %         line([1 l],[-0.1 -0.1],'Color','red')
+% %         hold on
+% %         line([1 l],[-0.2 -0.2],'Color','red')
+% %         hold on
+% %         line([1 l],[-0.3 -0.3],'Color','red')
+% %         hold on
+% %         line([1 l],[-0.4 -0.4],'Color','red')
+% %         hold on
+% %         line([1 l],[-0.5 -0.5], 'Color','red')
+% %         title('left ankle')
+%        
+%     elseif left_ankle_2(i)<left_ankle_2(i-1) && left_ankle_2(i-1)>left_ankle_2(i-4)%si on a un maximum
+%         if (sub.gender=='M' && BMI<21 && left_ankle_2(i)>0.84) || (sub.gender=='F' && left_ankle_2(i)>1.28) || (sub.gender=='M'&&BMI>21 && left_ankle_2(i)>0.65)
+%             %on est sur d'etre sur du plat ou descente
+%             ankleIsNot2=true;
+%         else
+%             % sinon on n'a pas de nouvelles infos
+%             compteur_ankle = 0;
+%         end
+%         
+%         
+%     end
+
     if i<700 %On suppose que le patient est sur du plat pendant au moins 7 sec
         a_ankle=a_ankle+left_ankle(i); % pour pouvoir calculer la valeur moyenne
-        ankleChangement=true; % on est sur du plat
-        if i>2 && left_ankle(i)<left_ankle(i-1) && left_ankle(i-1)>left_ankle(i-2)%si c'est un sommet
-            b_ankle = b_ankle+1;
-            sommet_plat_ankle(b_ankle)=left_ankle(i);%matrice avec valeur des sommets
-        elseif i>2 && left_ankle(i)>left_ankle(i-1) && left_ankle(i-1)<left_ankle(i-2)%si c'est un minima
-            c_ankle = c_ankle+1;
-            minima_plat_ankle(c_ankle)=left_ankle(i);%matrice avec valeur des minima
+        ankleIsNot2=true; % on est sur du plat
+        if left_ankle(i)>sommet_plat_ankle_max
+            sommet_plat_ankle_max = left_ankle(i);
+        elseif left_ankle(i)<sommet_plat_ankle_min
+            sommet_plat_ankle_min = left_ankle(i);
         end
     elseif i==700
         a_ankle=(a_ankle+left_ankle(i))/700;%valeur moyenne plat
-        ankleChangement=true;
-        moyenne_sommet_plat_ankle = mean(sommet_plat_ankle);%valeur moyenne sommets sur plat
-        moyenne_minima_plat_ankle = mean(minima_plat_ankle);%valeur moyenne minima sur plat
-        left_ankle_2 = (left_ankle-a_ankle)/(moyenne_sommet_plat_ankle-moyenne_minima_plat_ankle);%on normalise la fonction (? faire au fur et ? mesure dans la version finale!!!!!)
-%         figure
-%         plot(left_ankle_2)
-%         hold on
-%         line([1 l],[0.1 0.1],'Color','red')
-%         hold on
-%         line([1 l],[0.2 0.2],'Color','red')
-%         hold on
-%         line([1 l],[0.3 0.3],'Color','red')
-%         hold on
-%         line([1 l],[0.4 0.4],'Color','red')
-%         hold on
-%         line([1 l],[0.5 0.5],'Color','red')
-%         hold on
-%         line([1 l],[0.6 0.6],'Color','red')
-%         hold on
-%         line([1 l],[-0.1 -0.1],'Color','red')
-%         hold on
-%         line([1 l],[-0.2 -0.2],'Color','red')
-%         hold on
-%         line([1 l],[-0.3 -0.3],'Color','red')
-%         hold on
-%         line([1 l],[-0.4 -0.4],'Color','red')
-%         hold on
-%         line([1 l],[-0.5 -0.5], 'Color','red')
-%         title('left ankle')
-       
+        ankleIsNot2=true;
+        left_ankle_2 = (left_ankle-a_ankle)/(sommet_plat_ankle_max-sommet_plat_ankle_min);%on normalise la fonction
+        figure
+        plot(left_ankle_2)
+        title('left ankle-2')
     elseif left_ankle_2(i)<left_ankle_2(i-1) && left_ankle_2(i-1)>left_ankle_2(i-4)%si on a un maximum
         if (sub.gender=='M' && BMI<21 && left_ankle_2(i)>0.84) || (sub.gender=='F' && left_ankle_2(i)>1.28) || (sub.gender=='M'&&BMI>21 && left_ankle_2(i)>0.65)
             %on est sur d'etre sur du plat ou descente
-            ankleChangement=true;
+            ankleIsNot2=true;
         else
             % sinon on n'a pas de nouvelles infos
             compteur_ankle = 0;
         end
-        
-        
+    elseif std(left_ankle_2(i-150:i))*BMI- std(left_ankle_2(1:150))*BMI > 2.1
+        if answerFinale(i-1) == 2 && compteur <100 %ne peut pas redescendre dans la seconde quand il est sur du plat
+            compteur_ankle = compteur_ankle+1;
+        else
+            ankleIs3 = true;
+            compteur_ankle = 0;
+        end
     end
-    %
-    %     if i<700 %On suppose que le patient est sur du plat pendant au moins 7 sec
-    %        a_ankle=a_ankle+left_ankle(i); % pour pouvoir calculer la valeur moyenne
-    %        ankleChangement=true; % on est sur du plat
-    %        if left_ankle(i)>sommet_plat_ankle_max
-    %            sommet_plat_ankle_max = left_ankle(i);
-    %        elseif left_ankle(i)<sommet_plat_ankle_min
-    %            sommet_plat_ankle_min = left_ankle(i);
-    %        end
-    %     elseif i==700
-    %             a_ankle=(a_ankle+left_ankle(i))/700;%valeur moyenne plat
-    %             ankleChangement=true;
-    %             left_ankle_2 = (left_ankle-a)/(sommet_plat_ankle_max-sommet_plat_ankle_min);%on normalise la fonction (? faire au fur et ? mesure dans la version finale!!!!!)
-    %             figure
-    %             plot(left_ankle_2)
-    %             title('left_ankle-2')
-    %     elseif left_ankle_2(i) > 0.3
-    %         ankleChangement = true;
-    %     elseif left_ankle_2(i) < -0.6
-    %         if ankleChangement == 3 && compteur_ankle2 < 100
-    %             compteur_ankle2 = compteur_ankle2+1;
-    %         else
-    %             ankleChangement = 2;
-    %             compteur_ankle2 = 0;
-    %         end
-    %     elseif std(left_ankle_2(i-150:i))*BMI- std(left_ankle_2(1:150))*BMI < -4
-    %         if ankleChangement == 2 && compteur <100
-    %             compteur_ankle = compteur_ankle+1;
-    %         else
-    %             ankleChangement = 3;
-    %             compteur_ankle = 0;
-    %         end
-    %     end
+    
     %% Genou Gauche
     
     if i>700
@@ -228,10 +230,13 @@ for i=1:l
         answer=[answer 2 3];
         kneeChangement=false;
     end
-    if ankleChangement
+    if ankleIsNot2
         answer=[answer 1 3];
-        ankleChangement=false; 
+        ankleIsNot2=false;
     end
+%     if ankleIs3
+%         answer = [answer 3];
+%     end
 
     [t1,t2,t3] = mode(answer);
     t3 = cell2mat(t3);
@@ -241,32 +246,32 @@ for i=1:l
         answerFinale(i) = t1; %...sinon on prend la valeur qui apparait le plus souvent
     end
 end
-% figure
-% plot(time_vect,answerFinale, '.-b')
-% hold on
-% plot(time_vect,true_manoeuvre, '--r')
-% %figure
-% %plot(right_hip)
+figure
+plot(time_vect,answerFinale, '.-b')
+hold on
+plot(time_vect,true_manoeuvre, '--r')
+%figure
+%plot(right_hip)
 % 
-% q = 150;
-% l = length(time_vect);
-% movingSTD = zeros(1, l-q);
-% for p= q+1:l
-%     movingSTD(p-q) = std(left_hip_2(p-q:p))*BMI;
-% end
-% figure
-% plot(movingSTD-movingSTD(1))
-% hold on
-% %line([1 l-q],[15 15],'Color','red')
-% grid on
-% title('standard dev')
-% 
-% for p= q+1:l
-%     movingSTD(p-q) = std(right_hip_2(p-q:p))*BMI;
-% end
-% figure
-% plot(movingSTD-movingSTD(1))
-% hold on
-% %line([1 l-q],[15 15],'Color','red')
-% grid on
-% title('standard dev')
+q = 170;
+l = length(time_vect);
+movingSTD = zeros(1, l-q);
+for p= q+1:l
+    movingSTD(p-q) = std(left_hip_2(p-q:p))*BMI;
+end
+figure
+plot(time_vect(171:end),movingSTD-movingSTD(1))
+hold on
+%line([1 l-q],[15 15],'Color','red')
+grid on
+title('left standard dev')
+
+for p= q+1:l
+    movingSTD(p-q) = std(right_hip_2(p-q:p))*BMI;
+end
+figure
+plot(time_vect(171:end),movingSTD-movingSTD(1))
+hold on
+%line([1 l-q],[15 15],'Color','red')
+grid on
+title('right standard dev')
